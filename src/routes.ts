074a1175af929f6b9e527a5776163d5d00e387fc
Whiffmaster <85064127+Whiffmaster@users.noreply.github.com>
2025-04-27 19:06:11 +0200

    feat: Add YouTube streaming utility import to routes setup

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import {FastifyInstance} from "fastify";
 import {healthService} from "./services/health.service";
+import streamYT from "../utils/YTStream";
 
 export function setupRoutes(fastify: FastifyInstance) {
   fastify.get('/health', {}, async (request, reply) => {
     return {health: healthService.getHealth()}
   })
+
 }
