2f0033c9e4f3bac0f6071606f36162c197076626
Matěj Pešl <matejpesl1@gmail.com>
2025-04-27 18:18:32 +0200

    feat: add fastify

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("hey");
+// import dotenv
+import dotenv from 'dotenv'
+import Fastify from 'fastify'
+import * as assert from "node:assert";
+
+dotenv.config();
+
+const PORT = process.env["PORT"] as unknown as number ?? 3000;
+assert(Number.isInteger(PORT));
+
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler(request, reply) {
+  return {hello: 'world'}
+})
+
+// Run the server!
+try {
+  console.log(`Running server on port ${PORT}...`);
+  await fastify.listen({port: PORT})
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
