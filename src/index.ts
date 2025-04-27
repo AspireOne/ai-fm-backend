99b2feda928cdfc4ced8550a4564d65e376da376
Whiffmaster <85064127+Whiffmaster@users.noreply.github.com>
2025-04-27 23:18:51 +0200

    chore: Update default port from 5005 to 5006 in index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,19 @@ import {setupAppRoutes} from "./controllers/app.controller";
 
 dotenv.config();
 
-const PORT = Number(process.env["PORT"] ?? 5005);
+const PORT = Number(process.env["PORT"] ?? 5006);
 assert(Number.isInteger(PORT));
 
 const fastify = Fastify({
   logger: true
 })
 
+
 await fastify.register(cors, {
   origin: 'http://localhost:3001'
 });
+
+try {
   console.log(`Initializing routes...`);
   setupAppRoutes(fastify);
   console.log(`Running server on port ${PORT}...`);
