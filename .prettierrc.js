8c3f2c800b7b07fba83d8a1b0a07565d52e22614
Matěj Pešl <matejpesl1@gmail.com>
2025-04-28 13:15:57 +0200

    fix .prettierrc.js

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config} */
-export default {
-  singleQuote: false,
-  endOfLine: "auto",
-  tabWidth: 2,
-  printWidth: 90,
-  useTabs: false,
-  trailingComma: "all",
-};
