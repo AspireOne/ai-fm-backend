ae67971d65888239d6a8e6964c3dd5b874e41bc7
Matěj Pešl <matejpesl1@gmail.com>
2025-04-28 02:19:00 +0200

    feat: add 'downloadedFilesDir' path to OptionalPaths

--- src/providers/paths.ts
+++ src/providers/paths.ts
@@ -5,20 +5,24 @@ import {detectProjectRoot} from "../utils/detect-project-root.utils";
 
 const PROJECT_ROOT = detectProjectRoot();
 
+/** Pre-defined file/directory paths for the project. */
 export const Paths = {
+  /** Root directory of the project (where for example package.json resides). */
   projectRoot: PROJECT_ROOT,
+  /** Directory that contains static resources. */
   resourcesDir: join(PROJECT_ROOT, "src", "resources"),
+  /** Directory that contains static sweepers (resources). */
   sweepersDir: join(PROJECT_ROOT, "src", "resources", "sweepers"),
+  /** DB Migrations directory. */
   migrationsDir: join(PROJECT_ROOT, "migrations"),
   // ... other paths
 } as const;
 
+/** Pre-defined file/directory paths for the project that are valid even if they do NOT exist. */
 export const OptionalPaths = {
-  logsDir: join(PROJECT_ROOT, "logs"),
+  /** Directory that contains locally downloaded files not part of the project. */
+  downloadedFilesDir: join(PROJECT_ROOT, "downloaded-files"),
 };
-
-// Type helper
-export type PathKey = keyof typeof Paths;
 
 export const validatePredefinedPathsExistOrThrow = async (): Promise<void> => {
   const errors: Error[] = [];
