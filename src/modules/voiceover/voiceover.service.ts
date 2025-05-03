61a8ce439ff0ba49e1718aee4ca88a88274520dc
Matěj Pešl <matejpesl1@gmail.com>
2025-05-04 01:38:27 +0200

    feat: improve Cara voice

--- src/modules/voiceover/voiceover.service.ts
+++ src/modules/voiceover/voiceover.service.ts
@@ -6,16 +6,19 @@ import { Block } from "../radio/radio.types";
 import assert from "node:assert";
 import fs from "fs";
 import audioFileManagerService from "../audio-file-manager/audio-file-manager.service";
+import { VoiceSettings } from "elevenlabs/api";
 
 const textGenerationModel = "openai/gpt-4.1-mini";
 const elevenlabsProps = {
   voice: "Cara",
-  model_id: "eleven_turbo_v2_5",
+  model_id: "eleven_multilingual_v2", //"eleven_turbo_v2_5",
   voice_settings: {
     stability: 0.34,
-    similarity_boost: 0.85,
+    similarity_boost: 0.8,
+    speed: 1.08,
+    use_speaker_boost: true,
     // style: 0.2 // consumes additional resources
-  },
+  } as VoiceSettings,
 };
 
 // We need previous voiceovers and prev/next song name.
@@ -41,6 +44,7 @@ async function generateVoiceoverText(props: CreateVoiceoverProps) {
 async function generateVoiceoverAudio(text: string) {
   return await elevenlabs.generate({
     ...elevenlabsProps,
+    output_format: "mp3_44100_128",
     text: text,
   });
 }
